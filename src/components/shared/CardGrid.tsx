034225780eb6dce59888eff5888e8a2ff84d2b99
ledemkam <ledemkam@gmail.com>
2024-12-11 09:01:52 +0100

    card implement

--- src/components/shared/CardGrid.tsx
+++ src/components/shared/CardGrid.tsx
@@ -1,4 +1,5 @@
 import { News } from "@/types";
+import NewsPageCard from "./NewsPageCard";
 
 type CardGridProps = {
   objects: News[] ;
@@ -9,9 +10,9 @@ const CardGrid = ({objects,mode}:CardGridProps) =>{
   console.log(mode);
   
 	return(
-    <div className="grid">
-       {objects.map((item,index) =>(
-        <img key={index} src={item.image_url} alt={item.title} />
+    <div className="grid grid-cols-1 gap-y-4 auto-rows-[600px] lg:auto-rows-[300px]">
+       {objects.map((item) =>(
+        <NewsPageCard key={item.id} news={item} />
        ))}
     </div>
   )
---
+++ src/components/shared/NewsPageCard.tsx
@@ -0,0 +1,34 @@
+import { News } from "@/types";
+import { CircleArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
+
+type PropsCard = {
+    news: News;
+}
+const NewsPageCard = ({news}: PropsCard) => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-x-4">
+        <div className="p-2 overflow-hidden lg:col-span-1 h-[300px] md:h-[400px] lg:h-full">
+            <Link to={news.url} target="_blank">
+               <img src={news.image_url} alt={news.title} className="h-full w-full object-cover"/>
+            </Link>
+        </div>
+        <div className="p-2 overflow-hidden lg:col-span-3 h-[300px] md:h-[200px] lg:h-full">
+            <p className="text-2xl font-bold">{news.title}</p>
+            <p>{news.publishedAt ? news.publishedAt.split("T")[0] : "Unknown date"}</p>
+            <p className="flex items-center">
+                <span>{news.news_site}</span>
+                <span>|</span>
+                <span className="flex gap-x-2">Read from source
+                <Link to={news.url} target="_blank">
+                 <CircleArrowRight className="hover:scale-150 transition-all" color="var(--clr-violet)" />
+                </Link>
+                </span>
+            </p>
+            <p className="mt-4">{news.summary}</p>
+        </div>
+    </div>
+  )
+}
+
+export default NewsPageCard
